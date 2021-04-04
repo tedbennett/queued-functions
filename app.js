3616b76b5905761db3413f9c2b16e5978ef55ada
Ted <teddsbennett@gmail.com>
2021-04-04 20:07:37 +0100

    S3 files by id, add search

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require('express');
 const logger = require('morgan');
 const { createServer } = require('http');
 const WebSocket = require('ws');
+require('dotenv').config();
 
+const { default: axios } = require('axios');
 const usersRouter = require('./routes/users');
 const sessionsRouter = require('./routes/sessions');
 const storageRouter = require('./routes/storage');
@@ -18,6 +20,53 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/users', usersRouter);
 app.use('/sessions', sessionsRouter);
 app.use('/storage', storageRouter);
+
+const spotifyCredentials = async () => {
+  const params = new URLSearchParams();
+  params.append('grant_type', 'client_credentials');
+  params.append('client_id', '1e6ef0ef377c443e8ebf714b5b77cad7');
+  params.append('client_secret', process.env.SPOTIFY_SECRET);
+  // const params = {
+  //   headers: {
+  //     Authorization: `Basic 1e6ef0ef377c443e8ebf714b5b77cad7:${process.env.SPOTIFY_SECRET}`,
+  //     'Content-Type': 'application/x-www-form-urlencoded',
+  //   },
+  // // };
+  // const urlencoded = new URLSearchParams();
+  // urlencoded.append('grant_type', 'client_credentials');
+  const header = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } };
+
+  const response = await axios.post('https://accounts.spotify.com/api/token', params, header)
+    .catch((err) => console.log(err));
+  return response.data.access_token;
+};
+
+const extractSong = (data) => {
+  if (data.items === undefined) return [];
+  return data.items.map((track) => ({
+    id: track.uri,
+    name: track.name,
+    album: track.album.name,
+    artist: track.artists[0].name,
+    image_url: track.album.images[0].url,
+  }));
+};
+
+app.get('/search', async (req, res) => {
+  console.log('Searching');
+  const { query } = req.query;
+  const accessToken = await spotifyCredentials();
+  const url = `https://api.spotify.com/v1/search?q=${query}&type=track&limit=10`;
+
+  const params = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
+  axios.get(url, params)
+    .then((tracks) => res.send(extractSong(tracks.data.tracks)));
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/storage.js
+++ routes/storage.js
@@ -1,20 +1,21 @@
 const router = require('express').Router();
 const aws = require('aws-sdk');
-const crypto = require('crypto');
 
 aws.config.region = 'eu-west-1';
 
 require('dotenv').config();
 
-const getRandomFilename = () => crypto.randomBytes(16).toString('hex');
-
 router.get('/sign-s3', (req, res) => {
   const s3 = new aws.S3();
-  const fileName = getRandomFilename();
+  const { id } = req.query;
+  if (id === undefined) {
+    res.send(404);
+    return;
+  }
   const fileType = req.query['file-type'];
   const s3Params = {
     Bucket: process.env.S3_BUCKET,
-    Key: fileName,
+    Key: id,
     Expires: 60,
     ContentType: fileType,
     ACL: 'public-read',
@@ -27,7 +28,7 @@ router.get('/sign-s3', (req, res) => {
     }
     const returnData = {
       signedRequest: data,
-      url: `https://${process.env.S3_BUCKET}.s3.amazonaws.com/${fileName}`,
+      url: `https://${process.env.S3_BUCKET}.s3.amazonaws.com/${id}`,
     };
     res.json(returnData);
     res.end();
